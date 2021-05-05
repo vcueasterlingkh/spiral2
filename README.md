057c6a5cc0b3b485fe77267448dae4635fd57939
vcueasterlingkh <78060887+vcueasterlingkh@users.noreply.github.com>
2021-05-04 21:15:09 -0400

    final hw

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # spiral
 
-[![Python package](https://github.com/vcu-chfauerbach/spiral/actions/workflows/pytest.yml/badge.svg)](https://github.com/vcu-chfauerbach/spiral/actions/workflows/pytest.yml)
+[![Python package](https://github.com/vcueasterlingkh/spiral/actions/workflows/pytest.yml/badge.svg)](https://github.com/vcu-chfauerbach/spiral/actions/workflows/pytest.yml)
 
 What is the sum of the numbers on the diagonals in a 501 by 501 spiral formed in the same way?
--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,25 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
+    x = 1
+    counter = 0
+    incrt = 4
+    total = 0
+    for i in range(n):
+        i = n + 1
+        size = sqrt(i)
+
+    while n <= number ** 4
+        size += n
+        n +=  incrt
+        counter += 2
+        if counter == 8:
+            incrt += 4
+            counter = 0
+    
+
     return_value = n
     return return_value
+
+    if __name__ == "__main__":
+        n = 17
+        while n <= 102
+        answer = spiralize(n)
